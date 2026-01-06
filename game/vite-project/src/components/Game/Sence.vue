303c3b5cb8f8c3b05546e8412afbffef292a627f
石兴龙 <1003865572@qq.com>
2026-01-06 23:13:05 +0800

    1

---
+++ game/vite-project/src/components/Game/Sence.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+/*
+    0.1
+        固定10个单词
+    1.0
+        可以自定义单词
+*/
+
+</script>
+<template>
+  <div class="sence">
+    Sence
+  </div>
+</template>
+<style lang="scss">
+.sence {
+  height: 600px;
+  width: 350px;
+  background-image: url('./background.png');
+  background-size: 100% 100%;
+  border: 5px solid #0084ff;
+}
+</style>
---
+++ game/vite-project/src/components/Game/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import Sence from './Sence.vue'
+</script>
+<template>
+    <div class="game">
+        <Sence />
+    </div>
+</template> 
+<style lang="scss">
+.game {
+    height: 600px;
+    width: 350px;
+    margin: auto;
+}
+</style>
